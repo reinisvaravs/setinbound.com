9277776c017e6fe21a4baf48578fb39bead6afc1
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-05-31 14:47:07 +0300

    feature section update

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -1,4 +1,4 @@
-import SectionTitle from "../Common/SectionTitle";
+// import SectionTitle from "../Common/SectionTitle";
 import SingleFeature from "./SingleFeature";
 import featuresData from "./featuresData";
 
@@ -6,11 +6,11 @@ const Features = () => {
   return (
     <section className="pb-8 pt-20 dark:bg-dark lg:pb-[70px] lg:pt-[120px]">
       <div className="container">
-        <SectionTitle
+        {/* <SectionTitle
           subtitle="Features"
           title="Main Features Of Play"
           paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
-        />
+        /> */}
 
         <div className="-mx-4 mt-12 flex flex-wrap lg:mt-20">
           {featuresData.map((feature, i) => (
