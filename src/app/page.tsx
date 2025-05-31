a03bdc593e810f1f9fc704d7b06f7284ca6880d0
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-05-31 14:30:11 +0300

    removed pricing section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Contact from "@/components/Contact";
 import Faq from "@/components/Faq";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
-// import Pricing from "@/components/Pricing";
+import Pricing from "@/components/Pricing";
 import Team from "@/components/Team";
 import Testimonials from "@/components/Testimonials";
 import { getAllPosts } from "@/utils/markdown";
@@ -15,7 +15,8 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Play Next.js - SaaS Starter Kit and Boilerplate for Next.js",
-  description: "Free Next.js SaaS Boilerplate and Starter Kit designed and built for SaaS startups. It comes with all necessary integrations, pages, and components you need to launch a feature-rich SaaS websites.",
+  description:
+    "Free Next.js SaaS Boilerplate and Starter Kit designed and built for SaaS startups. It comes with all necessary integrations, pages, and components you need to launch a feature-rich SaaS websites.",
 };
 
 export default function Home() {
@@ -28,7 +29,7 @@ export default function Home() {
       <Features />
       <About />
       <CallToAction />
-      {/* <Pricing /> */}
+      <Pricing />
       <Testimonials />
       <Faq />
       <Team />
