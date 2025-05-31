63689a65e1f5c93f303b63b5c6e2b6cfa90c4e1e
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-05-31 14:27:57 +0300

    initial

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,358 @@
+"use client";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import menuData from "./menuData";
+
+const Header = () => {
+  const pathUrl = usePathname();
+  // Navbar toggle
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const navbarToggleHandler = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  // Sticky Navbar
+  const [sticky, setSticky] = useState(false);
+  const handleStickyNavbar = () => {
+    if (window.scrollY >= 80) {
+      setSticky(true);
+    } else {
+      setSticky(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleStickyNavbar);
+  });
+
+  // submenu handler
+  const [openIndex, setOpenIndex] = useState(-1);
+  const handleSubmenu = (index: any) => {
+    if (openIndex === index) {
+      setOpenIndex(-1);
+    } else {
+      setOpenIndex(index);
+    }
+  };
+
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <>
+      <header
+        className={`ud-header left-0 top-0 z-40 flex w-full items-center ${
+          sticky
+            ? "shadow-nav fixed z-[999] border-b border-stroke bg-white/80 backdrop-blur-[5px] dark:border-dark-3/20 dark:bg-dark/10"
+            : "absolute bg-transparent"
+        }`}
+      >
+        <div className="container">
+          <div className="relative -mx-4 flex items-center justify-between">
+            <div className="w-60 max-w-full px-4">
+              <Link
+                href="/"
+                className={`navbar-logo block w-full ${
+                  sticky ? "py-2" : "py-5"
+                } `}
+              >
+                {pathUrl !== "/" ? (
+                  <>
+                    <Image
+                      src={`/images/logo/logo.svg`}
+                      alt="logo"
+                      width={240}
+                      height={30}
+                      className="header-logo w-full dark:hidden"
+                    />
+                    <Image
+                      src={`/images/logo/logo-white.svg`}
+                      alt="logo"
+                      width={240}
+                      height={30}
+                      className="header-logo hidden w-full dark:block"
+                    />
+                  </>
+                ) : (
+                  <>
+                    <Image
+                      src={`${
+                        sticky
+                          ? "/images/logo/logo.svg"
+                          : "/images/logo/logo-white.svg"
+                      }`}
+                      alt="logo"
+                      width={140}
+                      height={30}
+                      className="header-logo w-full dark:hidden"
+                    />
+                    <Image
+                      src={"/images/logo/logo-white.svg"}
+                      alt="logo"
+                      width={140}
+                      height={30}
+                      className="header-logo hidden w-full dark:block"
+                    />
+                  </>
+                )}
+              </Link>
+            </div>
+            <div className="flex w-full items-center justify-between px-4">
+              <div>
+                <button
+                  onClick={navbarToggleHandler}
+                  id="navbarToggler"
+                  aria-label="Mobile Menu"
+                  className="absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
+                >
+                  <span
+                    className={`relative my-1.5 block h-0.5 w-[30px] transition-all duration-300 ${
+                      navbarOpen ? " top-[7px] rotate-45" : " "
+                    } ${pathUrl !== "/" && "!bg-dark dark:!bg-white"} ${
+                      pathUrl === "/" && sticky
+                        ? "bg-dark dark:bg-white"
+                        : "bg-white"
+                    }`}
+                  />
+                  <span
+                    className={`relative my-1.5 block h-0.5 w-[30px] transition-all duration-300 ${
+                      navbarOpen ? "opacity-0 " : " "
+                    } ${pathUrl !== "/" && "!bg-dark dark:!bg-white"} ${
+                      pathUrl === "/" && sticky
+                        ? "bg-dark dark:bg-white"
+                        : "bg-white"
+                    }`}
+                  />
+                  <span
+                    className={`relative my-1.5 block h-0.5 w-[30px] transition-all duration-300 ${
+                      navbarOpen ? " top-[-8px] -rotate-45" : " "
+                    } ${pathUrl !== "/" && "!bg-dark dark:!bg-white"} ${
+                      pathUrl === "/" && sticky
+                        ? "bg-dark dark:bg-white"
+                        : "bg-white"
+                    }`}
+                  />
+                </button>
+                <nav
+                  id="navbarCollapse"
+                  className={`navbar absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white px-6 py-4 duration-300 dark:border-body-color/20 dark:bg-dark-2 lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 lg:dark:bg-transparent ${
+                    navbarOpen
+                      ? "visibility top-full opacity-100"
+                      : "invisible top-[120%] opacity-0"
+                  }`}
+                >
+                  <ul className="block lg:ml-8 lg:flex lg:gap-x-8 xl:ml-14 xl:gap-x-12">
+                    {menuData.map((menuItem, index) =>
+                      menuItem.path ? (
+                        <li key={index} className="group relative">
+                          {pathUrl !== "/" ? (
+                            <Link
+                              onClick={navbarToggleHandler}
+                              scroll={false}
+                              href={menuItem.path}
+                              className={`ud-menu-scroll flex py-2 text-base text-dark group-hover:text-primary dark:text-white dark:group-hover:text-primary lg:inline-flex lg:px-0 lg:py-6 ${
+                                pathUrl === menuItem?.path && "text-primary"
+                              }`}
+                            >
+                              {menuItem.title}
+                            </Link>
+                          ) : (
+                            <Link
+                              scroll={false}
+                              href={menuItem.path}
+                              className={`ud-menu-scroll flex py-2 text-base lg:inline-flex lg:px-0 lg:py-6 ${
+                                sticky
+                                  ? "text-dark group-hover:text-primary dark:text-white dark:group-hover:text-primary"
+                                  : "text-body-color dark:text-white lg:text-white"
+                              } ${
+                                pathUrl === menuItem?.path &&
+                                sticky &&
+                                "!text-primary"
+                              }`}
+                            >
+                              {menuItem.title}
+                            </Link>
+                          )}
+                        </li>
+                      ) : (
+                        <li className="submenu-item group relative" key={index}>
+                          {pathUrl !== "/" ? (
+                            <button
+                              onClick={() => handleSubmenu(index)}
+                              className={`ud-menu-scroll flex items-center justify-between py-2 text-base text-dark group-hover:text-primary dark:text-white dark:group-hover:text-primary lg:inline-flex lg:px-0 lg:py-6`}
+                            >
+                              {menuItem.title}
+
+                              <span className="pl-1">
+                                <svg
+                                  className={`duration-300 lg:group-hover:rotate-180`}
+                                  width="16"
+                                  height="17"
+                                  viewBox="0 0 16 17"
+                                  fill="none"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <path
+                                    d="M8.00039 11.9C7.85039 11.9 7.72539 11.85 7.60039 11.75L1.85039 6.10005C1.62539 5.87505 1.62539 5.52505 1.85039 5.30005C2.07539 5.07505 2.42539 5.07505 2.65039 5.30005L8.00039 10.525L13.3504 5.25005C13.5754 5.02505 13.9254 5.02505 14.1504 5.25005C14.3754 5.47505 14.3754 5.82505 14.1504 6.05005L8.40039 11.7C8.27539 11.825 8.15039 11.9 8.00039 11.9Z"
+                                    fill="currentColor"
+                                  />
+                                </svg>
+                              </span>
+                            </button>
+                          ) : (
+                            <button
+                              onClick={() => handleSubmenu(index)}
+                              className={`ud-menu-scroll flex items-center justify-between py-2 text-base lg:inline-flex lg:px-0 lg:py-6 ${
+                                sticky
+                                  ? "text-dark group-hover:text-primary dark:text-white dark:group-hover:text-primary"
+                                  : "text-white"
+                              }`}
+                            >
+                              {menuItem.title}
+
+                              <span className="pl-1">
+                                <svg
+                                  className={`duration-300 lg:group-hover:rotate-180`}
+                                  width="16"
+                                  height="17"
+                                  viewBox="0 0 16 17"
+                                  fill="none"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <path
+                                    d="M8.00039 11.9C7.85039 11.9 7.72539 11.85 7.60039 11.75L1.85039 6.10005C1.62539 5.87505 1.62539 5.52505 1.85039 5.30005C2.07539 5.07505 2.42539 5.07505 2.65039 5.30005L8.00039 10.525L13.3504 5.25005C13.5754 5.02505 13.9254 5.02505 14.1504 5.25005C14.3754 5.47505 14.3754 5.82505 14.1504 6.05005L8.40039 11.7C8.27539 11.825 8.15039 11.9 8.00039 11.9Z"
+                                    fill="currentColor"
+                                  />
+                                </svg>
+                              </span>
+                            </button>
+                          )}
+
+                          <div
+                            className={`submenu relative left-0 top-full w-[250px] rounded-sm bg-white p-4 transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark-2 lg:invisible lg:absolute lg:top-[110%] lg:block lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${
+                              openIndex === index ? "!-left-[25px]" : "hidden"
+                            }`}
+                          >
+                            {menuItem?.submenu?.map((submenuItem: any, i) => (
+                              <Link
+                                href={submenuItem.path}
+                                key={i}
+                                className={`block rounded px-4 py-[10px] text-sm ${
+                                  pathUrl === submenuItem.path
+                                    ? "text-primary"
+                                    : "text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary"
+                                }`}
+                              >
+                                {submenuItem.title}
+                              </Link>
+                            ))}
+                          </div>
+                        </li>
+                      ),
+                    )}
+                  </ul>
+                </nav>
+              </div>
+              <div className="flex items-center justify-end pr-16 lg:pr-0">
+                <button
+                  onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                  className="flex h-[40px] w-[40px] items-center justify-center rounded-full bg-primary/5 text-dark dark:bg-white/5 dark:text-white"
+                >
+                  <span className="dark:hidden">
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M12 17C14.7614 17 17 14.7614 17 12C17 9.23858 14.7614 7 12 7C9.23858 7 7 9.23858 7 12C7 14.7614 9.23858 17 12 17Z"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M12 1V3"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M12 21V23"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M4.22 4.22L5.64 5.64"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M18.36 18.36L19.78 19.78"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M1 12H3"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M21 12H23"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M4.22 19.78L5.64 18.36"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M18.36 5.64L19.78 4.22"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </span>
+                  <span className="hidden dark:inline-block">
+                    <svg
+                      viewBox="0 0 23 23"
+                      className={`h-[30px] w-[30px] fill-current text-dark dark:hidden ${
+                        !sticky && pathUrl === "/" && "text-white"
+                      }`}
+                    >
+                      <g clipPath="url(#clip0_40_125)">
+                        <path d="M16.6111 15.855C17.591 15.1394 18.3151 14.1979 18.7723 13.1623C16.4824 13.4065 14.1342 12.4631 12.6795 10.4711C11.2248 8.47905 11.0409 5.95516 11.9705 3.84818C10.8449 3.9685 9.72768 4.37162 8.74781 5.08719C5.7759 7.25747 5.12529 11.4308 7.29558 14.4028C9.46586 17.3747 13.6392 18.0253 16.6111 15.855Z" />
+                      </g>
+                    </svg>
+                  </span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
