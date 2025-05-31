f81f6dcd8f527035f4f807a2b0f8135a8666b26e
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-05-31 14:38:20 +0300

    fixed theme btn

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -260,7 +260,7 @@ const Header = () => {
                   onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
                   className="flex h-[40px] w-[40px] items-center justify-center rounded-full bg-primary/5 text-dark dark:bg-white/5 dark:text-white"
                 >
-                  <span className="dark:hidden">
+                  {theme === "dark" ? (
                     <svg
                       width="24"
                       height="24"
@@ -332,19 +332,16 @@ const Header = () => {
                         strokeLinejoin="round"
                       />
                     </svg>
-                  </span>
-                  <span className="hidden dark:inline-block">
+                  ) : (
                     <svg
                       viewBox="0 0 23 23"
-                      className={`h-[30px] w-[30px] fill-current text-dark dark:hidden ${
-                        !sticky && pathUrl === "/" && "text-white"
-                      }`}
+                      className="h-[30px] w-[30px] fill-current"
                     >
                       <g clipPath="url(#clip0_40_125)">
                         <path d="M16.6111 15.855C17.591 15.1394 18.3151 14.1979 18.7723 13.1623C16.4824 13.4065 14.1342 12.4631 12.6795 10.4711C11.2248 8.47905 11.0409 5.95516 11.9705 3.84818C10.8449 3.9685 9.72768 4.37162 8.74781 5.08719C5.7759 7.25747 5.12529 11.4308 7.29558 14.4028C9.46586 17.3747 13.6392 18.0253 16.6111 15.855Z" />
                       </g>
                     </svg>
-                  </span>
+                  )}
                 </button>
               </div>
             </div>
