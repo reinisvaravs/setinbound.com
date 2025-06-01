542ad0544b1103873321bda099440a11078aea31
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-06-01 15:36:23 +0300

    head

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,7 @@ import Hero from "@/components/Hero";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Play Next.js - SaaS Starter Kit and Boilerplate for Next.js",
-  description:
-    "Free Next.js SaaS Boilerplate and Starter Kit designed and built for SaaS startups. It comes with all necessary integrations, pages, and components you need to launch a feature-rich SaaS websites.",
+  title: "Lintra AI - AI Voice Agents",
 };
 
 export default function Home() {
