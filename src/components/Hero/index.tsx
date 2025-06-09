a17bd43ccb351b01bb1689f82172536877b702aa
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-06-09 21:02:30 +0300

    new cal link

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -43,7 +43,7 @@ const Hero = () => {
                   </li>
                   <li>
                     <Link
-                      href="https://cal.com/reinisvaravs/30min"
+                      href="https://cal.com/lintraai/30min"
                       target="_blank"
                       className="flex items-center gap-4 rounded-md bg-white/[0.12] px-6 py-[14px] text-base font-medium text-white transition duration-300 ease-in-out hover:bg-white hover:text-dark"
                     >
