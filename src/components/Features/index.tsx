aa58333a2c684116dbfa827ac3381fe1a513693d
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-06-10 03:19:26 +0300

    final colors

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -1,4 +1,3 @@
-// import SectionTitle from "../Common/SectionTitle";
 import SingleFeature from "./SingleFeature";
 import featuresData from "./featuresData";
 
@@ -6,12 +5,6 @@ const Features = () => {
   return (
     <section className="pb-8 pt-20 dark:bg-dark lg:pb-[70px] lg:pt-[120px]">
       <div className="container">
-        {/* <SectionTitle
-          subtitle="Features"
-          title="Main Features Of Play"
-          paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
-        /> */}
-
         <div className="-mx-4 mt-12 flex flex-wrap justify-evenly lg:mt-20">
           {featuresData.map((feature, i) => (
             <SingleFeature key={i} feature={feature} />
