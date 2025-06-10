89578701f17b35a2881ef978155dfb6a2bfee8ae
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-06-10 03:46:06 +0300

    favicon pretty

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,12 @@ export default function RootLayout({
 
   return (
     <html suppressHydrationWarning={true} className="!scroll-smooth" lang="en">
+      <head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+        <link rel="shortcut icon" href="/favicon.png" />
+      </head>
       <body>
         {loading ? (
           <PreLoader />
