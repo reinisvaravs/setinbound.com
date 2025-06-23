1e68b04ba19385fa2decf7fdc525944c161c8ee2
Reinis Varavs <reinisroberts.varavs2@gmail.com>
2025-06-23 16:26:30 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import CallToAction from "@/components/CallToAction";
 import ScrollUp from "@/components/Common/ScrollUp";
-import Faq from "@/components/Faq";
+{/*import Faq from "@/components/Faq";*/}
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import { Metadata } from "next";
