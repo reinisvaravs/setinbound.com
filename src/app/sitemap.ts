a6e34f61d3e712dc1882f787261cb8e16f08ee45
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-06-26 23:38:54 +0300

    fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,7 +11,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${baseUrl}/referal`,
+      url: `${baseUrl}/referral`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
