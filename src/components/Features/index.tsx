6ad3270a078ac356f0422363ed8c41bf39966478
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-06-28 18:40:18 +0300

    small fix

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -8,7 +8,7 @@ const Features = () => {
         className="hero-content wow fadeInUp mx-auto -mt-14 w-[80vw] max-w-[780px] border-b border-secondary-GRAY text-center sm:w-[70vw] md:w-[60vw] lg:w-[60vw] xl:w-[50vw]"
         data-wow-delay=".2s"
       >
-        <h1 className="mb-4 mt-16 text-xl font-extrabold uppercase leading-tight text-secondary-GRAY sm:mb-6 sm:mt-20 sm:text-xl md:text-2xl lg:mb-6 lg:mt-20 lg:text-2xl lg:leading-snug xl:text-3xl xl:leading-snug 2xl:text-4xl 2xl:leading-[1.2]">
+        <h1 className="mb-4 mt-16 text-2xl font-extrabold uppercase leading-tight text-secondary-GRAY sm:mb-6 sm:mt-20 sm:text-xl md:text-2xl lg:mb-6 lg:mt-20 lg:text-2xl lg:leading-snug xl:text-3xl xl:leading-snug 2xl:text-4xl 2xl:leading-[1.2]">
           GET A FREE AI RECEPTIONIST
         </h1>
         <p className="mx-auto mb-6 max-w-[450px] px-1 font-light leading-relaxed text-secondary-LIGHT_GRAY sm:-mt-3 sm:mb-9 sm:max-w-[500px] sm:text-base sm:leading-[1.44] md:max-w-[600px] md:text-base md:leading-[1.44] lg:-mt-3 lg:mb-9 lg:max-w-[600px] lg:text-base lg:leading-[1.44]">
