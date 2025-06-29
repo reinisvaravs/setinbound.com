139862f5f617bfa75faebf98d72e2a3ae0c5e678
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-06-29 19:44:20 +0300

    new favicons

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -48,7 +48,15 @@ export const metadata: Metadata = {
     site: "@setinbound.com",
   },
   icons: {
-    icon: "/favicon.png",
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+    other: [{ rel: "manifest", url: "/site.webmanifest" }],
   },
   viewport: {
     width: "device-width",
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,10 +34,25 @@ export default function RootLayout({
       lang="en"
     >
       <head>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
-        <link rel="apple-touch-icon" href="/favicon.png" />
-        <link rel="shortcut icon" href="/favicon.png" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>
         <ClientPreLoader>
