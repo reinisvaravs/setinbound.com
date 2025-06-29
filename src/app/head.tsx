5a573057b11b04c372f7e43fd780ee13c6fa7fc8
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-06-29 19:46:38 +0300

    better faivcons

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -53,10 +53,14 @@ export const metadata: Metadata = {
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
+    shortcut: "/favicon.ico",
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
-    other: [{ rel: "manifest", url: "/site.webmanifest" }],
+    other: [
+      { rel: "manifest", url: "/site.webmanifest" },
+      { rel: "mask-icon", url: "/safari-pinned-tab.svg", color: "#7F3DF4" },
+    ],
   },
   viewport: {
     width: "device-width",
