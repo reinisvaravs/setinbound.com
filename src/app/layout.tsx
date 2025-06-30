819bad82f8bda5dcecb47eef0bbc273badc938b2
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-06-30 20:26:19 +0300

    fix head?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,6 @@ export default function RootLayout({
       className={`!scroll-smooth ${inter.className}`}
       lang="en"
     >
-      <head></head>
       <body>
         <ClientPreLoader>
           <Header />
